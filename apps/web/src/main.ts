c36af3318176c3e112c03c49634622f7eea732d0
p0ogars <cafipeyrw5305@outlook.com>
2026-05-17 16:54:53 +0800

    feat: wire up app + streaming in web demo entrypoint

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,1 +1,11 @@
-console.log('Harmony web demo loaded');
+import './app';
+import { createStreamingSession } from './streaming';
+
+// Preload AudioContext on user interaction (autoplay policy)
+window.addEventListener(
+  'click',
+  () =&gt; {
+    void createStreamingSession().then((s) =&gt; s.stop());
+  },
+  { once: true }
+);
